c54c06080f7eed1f984c19937b8c9fb16a2b4683
Higor Bereta <higor.bereta@gmail.com>
2026-09-21 18:11:39 -0300

    Aula 04: Templates, cookies e middleware de autenticação

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 from src.router import cliente as cliente_router
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(
     title="Techlog Soluction API - ALURA PROJECT",
@@ -8,27 +13,26 @@ app = FastAPI(
     version="0.0.1"
 )
 
-app.include_router(cliente_router.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+app.include_router(cliente_router.router)
+app.include_router(cliente_router.front_router)
 
 @app.get("/health")
 async def health():
     return {"status": "API is healthy"}
 
-@app.get("/front", response_class=HTMLResponse)
-async def front():
-    html_content = """
-    <html>
-        <head>
-            <title>Techlog Soluction</title>
-        </head>
-        <body>
-            <h1>Bem-vindo à Techlog Soluction</h1>
-            <p>Esta é a página inicial da API.</p>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
+@app.get("/", response_class=HTMLResponse)
+async def front_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "titulo": "Techlog Soluction API - ALURA PROJECT",
+            "descricao": "Projeto aula FAST API da Alura",
+            "versao": "0.0.1",
+        },
+    )
 
 @app.get("/jsontest", response_class=JSONResponse)
 async def jsontest():
--- src/router/cliente.py
+++ src/router/cliente.py
@@ -1,12 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from src.database.client_repository import ClientRepository
 from src.dependencies import get_client_repository
 from src.cliente import Cliente
 
 router = APIRouter(
+    prefix="/api/clientes",
+)
+
+front_router = APIRouter(
     prefix="/clientes",
 )
+
+templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_model=list[Cliente])
 async def get_clientes(client_repository: Annotated['ClientRepository', Depends(get_client_repository)]):
@@ -43,3 +54,47 @@ async def delete_cliente(client_id: int, client_repository: Annotated['ClientRep
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
     
     return None
+
+@front_router.get("/", response_class=HTMLResponse)
+async def front_page(request: Request, cliente_repository: Annotated['ClientRepository', Depends(get_client_repository)]):
+    clientes = await cliente_repository.list_client()
+    return templates.TemplateResponse(
+        request=request,
+        name="clientes.html",
+        context={
+            "request": request,
+            "clientes": clientes,
+            "titulo": "Lista de Clientes",
+            "descricao": "Esta é a lista de clientes cadastrados.",
+        },
+    )
+
+@front_router.get("/novo", response_class=HTMLResponse)
+async def front_new_client(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="clientes-form.html",
+        context={
+            "request": request,
+            "titulo": "Novo Cliente",
+            "descricao": "Formulário para cadastrar um novo cliente.",
+        },
+    )
+
+@front_router.get("/{client_id}", response_class=HTMLResponse)
+async def front_page_cliente(client_id: int, request: Request, cliente_repository: Annotated['ClientRepository', Depends(get_client_repository)]):
+    cliente = await cliente_repository.get_client_by_id(client_id)
+
+    if not cliente:
+        raise HTTPException(status_code=404, detail="Cliente não encontrado")
+    
+    return templates.TemplateResponse(
+        request=request,
+        name="clientes-form.html",
+        context={
+            "request": request,
+            "cliente": cliente,
+            "titulo": f"Detalhes do Cliente {cliente.nome}",
+            "descricao": f"Informações detalhadas sobre o cliente {cliente.nome}.",
+        },
+    )
